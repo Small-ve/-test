1af00463a08b6b7c1216b38e1a8161db71440ded
weiyi <2390230934@qq.com>
2020-09-28 15:04:22 +0800

    9-28小程序

---
+++ pages/index/index.js
@@ -0,0 +1,76 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg: '初始化的测试数据'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(this)
+    setTimeout(()=>{
+      this.setData({ msg: '修改后的数据' })
+      console.log(this.data.msg)
+    },3000)
+  },
+  handleParent(){
+    console.log('parent')
+  },
+  handleChildren(){
+    console.log('child')
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
